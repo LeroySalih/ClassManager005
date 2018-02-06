08f73ace641d7a2881c0acd9f6b34a22da679978
Leroy Salih <Leroy.Salih@gmail.com>
2018-02-06 19:23:09 +0300

    Initial Commit

---
+++ ClassManager005/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassManager005
+//
+//  Created by Leroy Salih on 06/02/2018.
+//  Copyright © 2018 Leroy Salih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
