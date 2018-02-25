b7634ad0133bfce1a9ea57adbbf98b54d6545e7a
Leroy Salih <Leroy.Salih@gmail.com>
2018-02-25 08:06:45 +0300

    COntinued refsactoring Views into VC

--- Controllers/PlannerPageHeaderVC.swift
+++ Controllers/PlannerPageHeaderVC.swift
@@ -19,7 +19,7 @@ class PlannerPageHeaderVC : UIViewController {
     
     var classLabel:UILabel = {
         
-        var label:UILabel = UILabel()
+        var label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = .yellow
         label.textColor = .black
@@ -29,7 +29,7 @@ class PlannerPageHeaderVC : UIViewController {
     
     var roomLabel:UILabel = {
         
-        var label:UILabel = UILabel()
+        var label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = .yellow
         label.textColor = .black
@@ -126,18 +126,25 @@ class PlannerPageHeaderVC : UIViewController {
         guard let ps = plannerSlot else { return }
         
         classLabel.text = ps.className
-        classLabel.sizeToFit()
+        classLabel.font = UIFont(name: "Avenir", size: 48)
+        classLabel.textAlignment  = .center
+        //classLabel.sizeToFit()
         
         roomLabel.text = ps.roomName
-        roomLabel.sizeToFit()
+        roomLabel.textAlignment = .center
+        roomLabel.font = UIFont(name: "Avenir", size: 20)
+        //roomLabel.sizeToFit()
         
         subjectLabel.text = ps.subject
+        subjectLabel.font = UIFont(name: "Avenir", size: 20)
         subjectLabel.sizeToFit()
         
         unitLabel.text = ps.unit
+        unitLabel.font = UIFont(name: "Avenir", size: 20)
         unitLabel.sizeToFit()
         
-        lessonLabel.text = ps.unit 
+        lessonLabel.text = ps.unit
+        lessonLabel.font = UIFont(name: "Avenir", size: 20)
         lessonLabel.sizeToFit()
         
     }
@@ -148,12 +155,17 @@ class PlannerPageHeaderVC : UIViewController {
         
         let constraints = [
             
-            view.heightAnchor.constraint(equalToConstant: 70),
+            view.heightAnchor.constraint(equalToConstant: 100),
             //label.heightAnchor.constraint(equalToConstant: 50),
+            headerClassStackView.widthAnchor.constraint(equalToConstant: 100),
             
             headerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             headerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            //classLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            //roomLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            
         ]
         
         constraints.forEach { (constraint) in
